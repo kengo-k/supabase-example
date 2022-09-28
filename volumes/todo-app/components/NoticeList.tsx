eb0d9ae39bd55dba1bf193a4021fd85063a27882
kengo-k <kurobanekengo@gmail.com>
2022-09-28 23:05:52 +0900

    ひとまずタスクのNoticeのCRUDを作成した

---
+++ volumes/todo-app/components/NoticeList.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+import { NoticeItem } from '@components/NoticeItem'
+import { Spinner } from '@components/Spinner'
+import { useQueryNotices } from '@hooks/useQueryNotices'
+
+export const NoticeList: FC = () => {
+  const { data: notices, status } = useQueryNotices()
+  if (status === 'loading') {
+    return <Spinner />
+  }
+  if (status === 'error') {
+    return <p>{'Error'}</p>
+  }
+  return (
+    <ul className="my-2">
+      {notices?.map((notice) => {
+        return (
+          <NoticeItem
+            key={notice.id}
+            id={notice.id}
+            content={notice.title}
+            user_id={notice.user_id}
+          />
+        )
+      })}
+    </ul>
+  )
+}
--- volumes/todo-app/pages/dashboard.tsx
+++ volumes/todo-app/pages/dashboard.tsx
@@ -1,7 +1,16 @@
-import { LogoutIcon } from '@heroicons/react/solid'
 import { NextPage } from 'next'
-import { Layout } from '../components/Layout'
-import { supabase } from '../utils/supabase'
+
+import { Layout } from '@components/Layout'
+import { NoticeForm } from '@components/NoticeForm'
+import { NoticeList } from '@components/NoticeList'
+import { TaskList } from '@components/TaskList'
+import { TaskForm } from '@components/Taskform'
+import {
+  LogoutIcon,
+  StatusOnlineIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/solid'
+import { supabase } from '@utils/supabase'
 
 const Dashboard: NextPage = () => {
   const signOut = () => {
@@ -13,6 +22,22 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+        <div>
+          <div className="my-3 flex justify-center">
+            <StatusOnlineIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <NoticeForm />
+          <NoticeList />
+        </div>
+      </div>
     </Layout>
   )
 }
